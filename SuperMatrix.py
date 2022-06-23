041579a273f09da6d6735668ebecafa1f1a62861
Noborita9 <joaquinbonora9@gmail.com>
2022-06-23 15:31:32 -0300

    add sarrus to SuperMatrix

--- SuperMatrix.py
+++ SuperMatrix.py
@@ -1,27 +1,50 @@
 import numpy as np
 class SuperMatrix():
-    def __init__(self, matrix=np.array([])) -> None:
+    def __init__(self, matrix) -> None:
         self.matrix = matrix
         self.size = np.shape(self.matrix)
+        self.delimeter = None
 
-    def ask_size(self):
-        finish = False
-        layer = 0
-        while not finish:
-            text = input(f"Ingrese los numeros de la capa {layer}. Ej:(4,1,3,2)\n~~> ")
-            numbers = np.array([int(num) for num in text.split(',')])
-            self.matrix = np.append(self.matrix, [numbers], axis=0)
-            isFinish = input("Quieres agregar otra capa?[Y/n]\n~~> ")
-            if isFinish in ["n", "N"]:
-                finish = True
+    def sarrus_resolve(self):
+        extended_matrix = np.array(self.matrix)
+        extended_matrix = np.append(extended_matrix, extended_matrix[:, 0:2], axis=1)
+        delimeter = 0
+        for repetition in range(3):
+            pos_val = np.array([])
+            neg_val = np.array([])
+            for number in range(3):
+                pos_val = np.append(pos_val, extended_matrix[number][number+repetition])
+                neg_val = np.append(neg_val, extended_matrix[number][-1-number-repetition])
+            p_hold = 1
+            n_hold = 1
+            for vp, vn in zip(pos_val, neg_val):
+                p_hold *= vp
+                n_hold *= vn
+            n_hold *= -1
+            delimeter += p_hold + n_hold
+        self.delimeter = delimeter
+        return delimeter
 
-    def get_size(self):
-        self.size = np.shape(self.matrix)
-        return self.size
+    def prop4(self):
+        mat_copy = np.array(self.matrix)
+        column = mat_copy[:, 0]
+        main_number = 0
+        for number in reversed(column):
+            if number != 0:
+                main_number = number
+                break
+        main_number_index = [len(column), len(self.matrix)]
+        col_sublist = column[:main_number_index[0]]
+        multipliers = [ mul/main_number for mul in col_sublist]
+        multipliers.append(1)
+        for number in range(main_number_index[0]-1):
+            self.matrix[number] = self.matrix[number] - (self.matrix[main_number_index[0]-1] * multipliers[number])
 
 
-matrix = SuperMatrix()
-matrix.ask_size()
-matrix_size = matrix.get_size()
-print(matrix.matrix)
-print(matrix_size)
+
+matrix = SuperMatrix(np.array([
+    [2,4,9],
+    [5,0,5],
+    [3,2,1]]))
+matrix.sarrus_resolve()
+print(matrix.delimeter)
